9a51e841f07415a94c847578fb503c4f351a2884
gabrielantonyxaviour <gabrielantony56@gmail.com>
2026-02-04 14:00:00 +0530

    Add node type barrel exports

---
+++ frontend/src/components/trade/nodes/index.ts
@@ -0,0 +1,5 @@
+export { TokenInputNode } from "./token-input-node";
+export { BridgeNode } from "./bridge-node";
+export { SwapNode } from "./swap-node";
+export { DepositNode } from "./deposit-node";
+export { OutputNode } from "./output-node";

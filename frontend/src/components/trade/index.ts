6eaca6fb39ce1468028f12a7179e5c93385d6878
gabrielantonyxaviour <gabrielantony56@gmail.com>
2026-02-04 16:50:00 +0530

    Add trade component barrel exports

---
+++ frontend/src/components/trade/index.ts
@@ -0,0 +1,6 @@
+export { ChatPanel } from "./chat-panel";
+export { FlowCanvas } from "./flow-canvas";
+export { DetailsPanel } from "./details-panel";
+
+// Re-export nodes
+export * from "./nodes";
